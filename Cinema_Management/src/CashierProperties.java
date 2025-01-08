3e9e0e59bfea698bc17df9e2b70f696ab62a36f3
Emirhan Kara <159714433+Emirhan-Kara@users.noreply.github.com>
2025-01-08 22:50:31 +0300

    Selected seats of the customer in the second scene is stored

--- Cinema_Management/src/CashierProperties.java
+++ Cinema_Management/src/CashierProperties.java
@@ -1,40 +1,85 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CashierProperties
 {
-    public static Movie selectMovie;
+    public static Movie selectedMovie;
 
     public static List<Product> currentProductPrices;
 
+
+    // arrenged at scene 2 and scene 3
+    protected static ArrayList<Integer> selectedSeatsAtScene2;
+    protected static List<Sessions> listedSessionsAtScene2;
     protected static double totalTicketPrice;
     protected static double totalTicketTax;
-
-    protected static double totalProductPrices;
-    protected static double totalProductTax;
 
     protected static Map<Integer, String> normalTicketMap;
     protected static Map<Integer, String> discountedTicketMap;
 
+    protected static Sessions selectedSession;
 
-    public static void prepareStaticVariables()
+    // arranged at scene 4
+    protected static int soldBeverageNum;
+    protected static int soldFoodNum;
+    protected static int soldToyNum;
+
+    protected static double totalProductPrice;
+    protected static double totalProductTax;
+
+    // calculated while creating the Bill
+    protected static double totalBillPrice;
+    protected static double totalBillTax;
+
+
+    protected static void prepareStaticVariables()
     {
-        selectMovie = null;
+        selectedMovie = null;
         currentProductPrices = DatabaseConnection.getProducts(null);
+
+        selectedSeatsAtScene2 = new ArrayList<>();
+        listedSessionsAtScene2 = new ArrayList<>();
 
         totalTicketPrice = 0;
         totalTicketTax = 0;
 
-        totalProductPrices = 0;
-        totalProductTax = 0;
-
         normalTicketMap = new HashMap<>();
         discountedTicketMap = new HashMap<>();
+
+        selectedSession = null;
+
+        soldBeverageNum = 0;
+        soldFoodNum = 0;
+        soldToyNum = 0;
+
+        totalProductPrice = 0;
+        totalProductTax = 0;
+
+        totalBillPrice = 0;
+        totalProductPrice = 0;
     }
 
-    public static void fetchCurrentProductPrices()
+    protected static void fetchCurrentProductPrices()
     {
         currentProductPrices = DatabaseConnection.getProducts(null);
+    }
+
+    protected static void createBillForScene5()
+    {
+        Bill newBill = new Bill();
+        if (normalTicketMap.size() > 0)
+            newBill.addTickets(normalTicketMap, selectedSession.getHallId(), selectedSession.getId(), false);
+        if (discountedTicketMap.size() > 0)
+            newBill.addTickets(discountedTicketMap, selectedSession.getHallId(), selectedSession.getId(), true);
+        if (soldFoodNum > 0)
+            newBill.addProduct(2, soldFoodNum);
+        if (soldBeverageNum > 0)
+            newBill.addProduct(3, soldBeverageNum);
+        if (soldToyNum > 0)
+            newBill.addProduct(4, soldToyNum);
+
+        newBill.finalizeBill();
     }
 }
