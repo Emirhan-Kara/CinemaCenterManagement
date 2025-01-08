1a74d0a0509b41d926330c22c4b6b8f66a0a9dc0
Nisa <155849485+cybernisa@users.noreply.github.com>
2025-01-09 00:32:51 +0300

    should be checked

--- Cinema_Management/src/Sessions.java
+++ Cinema_Management/src/Sessions.java
@@ -1,12 +1,12 @@
-import java.sql.Date;
 import java.sql.Time;
+import java.sql.Date;
 
 public class Sessions {
-    private int id;
-    private int hallId;
-    private int movieId;
-    private Date date;
-    private Time time;
+    public int id;
+    public int hallId;
+    public int movieId;
+    public Date date; // Fully qualified name
+    public Time time; // Fully qualified name
 
     public Sessions(int id, int hallId, int movieId, Date date, Time time) {
         this.id = id;
@@ -15,31 +15,50 @@ public class Sessions {
         this.date = date;
         this.time = time;
     }
-
-    // Getters
-    public int getId() {
+     // Getters and Setters
+     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public int getHallId() {
         return hallId;
     }
 
+    public void setHallId(int hallId) {
+        this.hallId = hallId;
+    }
+
     public int getMovieId() {
         return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
     }
 
     public Date getDate() {
         return date;
     }
 
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
     public Time getTime() {
         return time;
     }
 
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
     @Override
     public String toString() {
-        return "Sessions{" +
+        return "Session{" +
                 "id=" + id +
                 ", hallId=" + hallId +
                 ", movieId=" + movieId +
