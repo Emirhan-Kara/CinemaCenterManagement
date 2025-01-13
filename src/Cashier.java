6bd4d0bb591e198bb41728803499d158482c938b
Emirhan Kara <159714433+Emirhan-Kara@users.noreply.github.com>
2025-01-13 15:01:12 +0300

    Javadoc added Cashier.java

--- src/Cashier.java
+++ src/Cashier.java
@@ -1,7 +1,18 @@
-public class Cashier extends Employee {
+/**
+ * Cashier class for the user roles
+ * Extends to Employee class
+ */
+public class Cashier extends Employee
+{
+    /**
+     * Constructor
+     * @param id id
+     * @param username username
+     * @param firstname name
+     * @param lastname surname
+     * @param password password
+     */
     public Cashier(int id, String username, String firstname, String lastname, String password) {
         super(id, "cashier", username, firstname, lastname, password);
     }
-
-    // Cashier-specific methods can go here
 }
