4a9f65209d63b9c56035e0ae2417a9a06cd4288a
Emirhan Kara <159714433+Emirhan-Kara@users.noreply.github.com>
2025-01-13 15:01:36 +0300

    Javadoc added for CashierProperties.java

--- src/CashierProperties.java
+++ src/CashierProperties.java
@@ -1,46 +1,106 @@
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
+/**
+ * Parent class for all cashier controllers
+ * Holds the informations of each scene from the cashier to be able to use them if the user presses back
+ */
 public class CashierProperties
 {
+    /**
+     * selected movie in the first scene
+     */
     public static Movie selectedMovie;
 
+    /**
+     * product prices
+     */
     public static List<Product> currentProductPrices;
 
 
     // arrenged at scene 2 and scene 3
+    /**
+     * selected seats at the second scene
+     */
     protected static ArrayList<Integer> selectedSeatsAtScene2;
+
+    /**
+     * listed sessions at the second scene
+     */
     protected static List<Sessions> listedSessionsAtScene2;
+
+    /**
+     * total ticket prices
+     */
     protected static double totalTicketPrice;
+
+    /**
+     * total ticket taxes
+     */
     protected static double totalTicketTax;
 
+    /**
+     * seat id, customer name map for normal tickets
+     */
     protected static Map<Integer, String> normalTicketMap;
+
+    /**
+     * seat id, customer name map for discounted tickets
+     */
     protected static Map<Integer, String> discountedTicketMap;
 
+    /**
+     * selected session at the second scene
+     */
     protected static Sessions selectedSession;
 
     // arranged at scene 4
+    /**
+     * sold beverage number
+     */
     protected static int soldBeverageNum;
+
+    /**
+     * sold food number
+     */
     protected static int soldFoodNum;
+
+    /**
+     * sold toy number
+     */
     protected static int soldToyNum;
 
+    /**
+     * total product prices
+     */
     protected static double totalProductPrice;
+
+    /**
+     * calculated total product tax
+     */
     protected static double totalProductTax;
 
     // calculated while creating the Bill
+    /**
+     * total bill price
+     */
     protected static double totalBillPrice;
+
+    /**
+     * total bill tax
+     */
     protected static double totalBillTax;
 
+    /**
+     * Bill URI
+     */
     protected static String billURL;
 
+    /**
+     * To reset or create the static variables
+     */
     protected static void prepareStaticVariables()
     {
         selectedMovie = null;
@@ -68,11 +128,18 @@ public class CashierProperties
         totalProductPrice = 0;
     }
 
+    /**
+     * get all the product prices and related infos
+     */
     protected static void fetchCurrentProductPrices()
     {
         currentProductPrices = DatabaseConnection.getProducts(null);
     }
 
+    /**
+     * creates the bill with the bought products
+     * @throws Exception load scene exception
+     */
     protected static void createBillForScene5() throws Exception
     {
         Bill newBill = new Bill();
