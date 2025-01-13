cdad76133196e76e0e8cb899f2c6993d8c61dbee
MuratAlkaptan <131607763+MuratAlkaptan@users.noreply.github.com>
2025-01-13 08:14:12 +0300

    Update Admin.java
    Admin.java javadoc added

--- src/Admin.java
+++ src/Admin.java
@@ -1,8 +1,15 @@
-public class Admin extends Employee{
+public class Admin extends Employee {
+    /**
+     * Constructor for admin authentication
+     * @param id
+     * @param username
+     * @param firstname
+     * @param lastname
+     * @param password
+     * @return
+     */
     public Admin(int id, String username, String firstname, String lastname, String password) {
         super(id, "admin", username, firstname, lastname, password);
     }
-
-    // Admin-specific methods can go here
 }
 
