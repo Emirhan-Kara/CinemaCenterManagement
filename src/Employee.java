3937539a2a53832ac86efec919df16b341eed446
Emirhan Kara <159714433+Emirhan-Kara@users.noreply.github.com>
2025-01-13 15:02:27 +0300

    Javadoc added for Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,12 +1,47 @@
-public class Employee {
+/**
+ * Parent class for all the employees
+ */
+public class Employee
+{
+    /**
+     * id
+     */
     private int id;
+
+    /**
+     * user role
+     */
     private String userRole; // ENUM in database, String in Java
+
+    /**
+     * username
+     */
     private String username;
+
+    /**
+     * first name
+     */
     private String firstname;
+
+    /**
+     * surname
+     */
     private String lastname;
+
+    /**
+     * password
+     */
     private String password;
 
-    // Constructor
+    /**
+     * Overlaod constructor
+     * @param id id
+     * @param userRole role
+     * @param username username
+     * @param firstname name
+     * @param lastname surname
+     * @param password password
+     */
     public Employee(int id, String userRole, String username, String firstname, String lastname, String password) {
         this.id = id;
         this.userRole = userRole;
@@ -17,55 +52,105 @@ public class Employee {
     }
 
     // Getters and Setters
+    /**
+     * Id getter
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Id setter
+     * @param id id value
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Role getter
+     * @return role
+     */
     public String getUserRole() {
         return userRole;
     }
 
+    /**
+     * Role setter
+     * @param userRole role value
+     */
     public void setUserRole(String userRole) {
         this.userRole = userRole;
     }
 
+    /**
+     * Username getter
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Username setter
+     * @param username username value
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Name getter
+     * @return name
+     */
     public String getFirstname() {
         return firstname;
     }
 
+    /**
+     * Name setter
+     * @param firstname name value
+     */
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
 
+    /**
+     * Surname getter
+     * @return surname
+     */
     public String getLastname() {
         return lastname;
     }
 
+    /**
+     * Surname setter
+     * @param lastname surname value
+     */
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
 
+    /**
+     * Password getter
+     * @return password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Password setter
+     * @param password password value
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
-    // toString method
+    /**
+     * To string method for debug
+     */
     @Override
     public String toString() {
         return "Employee{" +
