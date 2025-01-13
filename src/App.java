002fec40677bf1927ef530a5573c44e2f8f2ab7f
Emirhan Kara <159714433+Emirhan-Kara@users.noreply.github.com>
2025-01-13 15:00:23 +0300

    Javadoc for App.java

--- src/App.java
+++ src/App.java
@@ -3,12 +3,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
+/**
+ * Main class for the application
+ */
 public class App extends Application
 {
+    /**
+     * Main stage that holds the scenes
+     */
     private static Stage primaryStage;
 
     
-    @Override // Override the start method in the Application class
+    /**
+     * Overried start method from the Application class
+     */
+    @Override
     public void start(Stage stage)
     {
         try
@@ -25,12 +34,21 @@ public class App extends Application
         }
     }
 
-    public static void loadScene(String fxmlPath) throws Exception
+    /**
+     * To switch scenes throughout the application
+     * @param fxmlPath name of the fxmlFile
+     * @throws Exception
+     */
+    public static void loadScene(String fxmlFileName) throws Exception
     {
-        FXMLLoader loader = new FXMLLoader(App.class.getResource(fxmlPath));
+        FXMLLoader loader = new FXMLLoader(App.class.getResource(fxmlFileName));
         primaryStage.setScene(new Scene(loader.load()));
     }
 
+    /**
+     * main method to call launch method
+     * @param args arguments
+     */
     public static void main(String[] args)
     {
         launch(args);
