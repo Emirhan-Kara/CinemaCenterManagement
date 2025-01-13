b4c4d65045b545682566fd7f1a001b73a495f3dd
Emirhan Kara <159714433+Emirhan-Kara@users.noreply.github.com>
2025-01-13 15:04:30 +0300

    Javadoc added for ManagerController.java

--- src/ManagerController.java
+++ src/ManagerController.java
@@ -6,29 +6,56 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Label;
 
+/**
+ * Manager main menu scene controller class
+ */
 public class ManagerController {
 
+    /**
+     * role label
+     */
     @FXML
     private Label roleLabel;
 
+    /**
+     * name surname label
+     */
     @FXML
     private Label nameSurnameLabel;
 
+    /**
+     * accounting button
+     */
     @FXML
     private Button economyButton;
 
+    /**
+     * employee operations button
+     */
     @FXML
     private Button employeeButton;
 
+    /**
+     * stocks button
+     */
     @FXML
     private Button inventoryButton;
 
+    /**
+     * logout button
+     */
     @FXML
     private Button logout;
 
+    /**
+     * prices button
+     */
     @FXML
     private Button productButton;
 
+    /**
+     * to set the name surname and role at top
+     */
     @FXML
     public void initialize()
     {
@@ -36,7 +63,13 @@ public class ManagerController {
         roleLabel.setText(LoginController.loggedEmployee.getUserRole());
     }
 
-    // Common method for handling button actions via MouseEvent or KeyEvent
+    /**
+     * to handle actions
+     * @param mouseEvent mouse event
+     * @param keyEvent keyboard event
+     * @param fxmlPath loaded scene
+     * @throws Exception for load scene
+     */
     public static void handleAction(MouseEvent mouseEvent, KeyEvent keyEvent, String fxmlPath) throws Exception {
         if (mouseEvent != null && mouseEvent.getButton() == MouseButton.PRIMARY) {
             App.loadScene(fxmlPath); // Mouse left click
@@ -45,53 +78,102 @@ public class ManagerController {
         }
     }
 
+    /**
+     * accounting button click action
+     * @param event evnt
+     * @throws Exception for load scene
+     */
     @FXML
     void economyClicked(MouseEvent event) throws Exception {
         handleAction(event, null, "ManagerAccountingScene.fxml");
     }
 
+    /**
+     * accounting button entered 
+     * @param event event
+     * @throws Exception for load scene
+     */
     @FXML
     void economyPressed(KeyEvent event) throws Exception {
         handleAction(null, event, "ManagerAccountingScene.fxml");
     }
 
+    /**
+     * employee button click action
+     * @param event evnt
+     * @throws Exception for load scene
+     */
     @FXML
     void employeeClicked(MouseEvent event) throws Exception {
         handleAction(event, null, "EmployeeListScene.fxml");
     }
 
+    /**
+     * employee button entered 
+     * @param event event
+     * @throws Exception for load scene
+     */
     @FXML
     void employeePressed(KeyEvent event) throws Exception {
         handleAction(null, event, "EmployeeListScene.fxml");
     }
 
+    /**
+     * stocks button click action
+     * @param event evnt
+     * @throws Exception for load scene
+     */
     @FXML
     void inventoryClicked(MouseEvent event) throws Exception {
         handleAction(event, null, "ManagerInventoryScene.fxml");
     }
 
+    /**
+     * stocks button entered 
+     * @param event event
+     * @throws Exception for load scene
+     */
     @FXML
     void inventoryPressed(KeyEvent event) throws Exception {
         handleAction(null, event, "ManagerInventoryScene.fxml");
     }
 
+    /**
+     * product button click action
+     * @param event evnt
+     * @throws Exception for load scene
+     */
     @FXML
     void productClicked(MouseEvent event) throws Exception {
         handleAction(event, null, "ManagerProductScene.fxml");
     }
 
+    /**
+     * prices button entered 
+     * @param event event
+     * @throws Exception for load scene
+     */
     @FXML
     void productPressed(KeyEvent event) throws Exception {
         handleAction(null, event, "ManagerProductScene.fxml");
     }
 
 
-    
+    /**
+     * logout button click action
+     * @param event evnt
+     * @throws Exception for load scene
+     */
     @FXML
     void logoutClicked(MouseEvent event) throws Exception {
         handleAction(event, null, "Login.fxml");
     }
 
+    /**
+     * logout button entered 
+     * @param event event
+     * @throws Exception for load scene
+     */
     @FXML
     void logoutPressed(KeyEvent event) throws Exception {
         handleAction(null, event, "Login.fxml");
