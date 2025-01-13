43bac04a6bcd2dcec21e4c795a41d4f65153ba18
Emirhan Kara <159714433+Emirhan-Kara@users.noreply.github.com>
2025-01-13 15:03:31 +0300

    Javadoc added for Manager.java

--- src/Manager.java
+++ src/Manager.java
@@ -1,8 +1,18 @@
-public class Manager extends Employee {
+/**
+ * Manager class, that extends Employee
+ */
+public class Manager extends Employee
+{
+    /**
+     * Overload constructor
+     * @param id id
+     * @param username username
+     * @param firstname name
+     * @param lastname surname
+     * @param password password
+     */
     public Manager(int id, String username, String firstname, String lastname, String password) {
         super(id, "manager", username, firstname, lastname, password);
     }
-
-    // Manager-specific methods can go here
 }
 
