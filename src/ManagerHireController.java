2e117b37d261c90043946183bd42f06656c7752c
Emirhan Kara <159714433+Emirhan-Kara@users.noreply.github.com>
2025-01-13 15:05:30 +0300

    Javadoc added for ManagerHireController.java

--- src/ManagerHireController.java
+++ src/ManagerHireController.java
@@ -7,84 +7,153 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
-public class ManagerHireController {
+/**
+ * Controller class for hiring option
+ */
+public class ManagerHireController
+{
 
+    /**
+     * back button
+     */
     @FXML
     private Button backButton;
 
+    /**
+     * logout button
+     */
     @FXML
     private Button logout;
 
+    /**
+     * name surname label at top
+     */
     @FXML
     private Label nameSurnameLabel;
 
+    /**
+     * role label at top
+     */
     @FXML
     private Label roleLabel;
 
     
 
-
+    /**
+     * surname input field
+     */
     @FXML
     private TextField surnameInput;
 
+    /**
+     * username input field
+     */
     @FXML
     private TextField usernameInput;
 
+    /**
+     * password input field
+     */
     @FXML
     private TextField passwordInput;
 
+    /**
+     * name input field
+     */
     @FXML
     private TextField nameInput;
 
+    /**
+     * add button
+     */
     @FXML
     private Button addButton;
 
+    /**
+     * error label
+     */
     @FXML
     private Label errorLabel;
 
+    /**
+     * choice box for role
+     */
     @FXML
     private ChoiceBox<String> roleInput;
 
 
+    /**
+     * name error field
+     */
     @FXML
     private Label nameErrorLabel;
 
+    /**
+     * surname error field
+     */
     @FXML
     private Label surnameErrorLabel;
 
+    /**
+     * username error field
+     */
     @FXML
     private Label usernameErrorLabel;
 
+    /**
+     * password error field
+     */
     @FXML
     private Label passwordErrorLabel;
 
+    /**
+     * event handler for back button click
+     * @param event mouns event
+     * @throws Exception for load scene
+     */
     @FXML
     void backClicked(MouseEvent event) throws Exception
     {
         ManagerController.handleAction(event, null, "ManagerMainMenu.fxml");
     }
 
+    /**
+     * event handler for back button press
+     * @param event keyboard event
+     * @throws Exception for load scene
+     */
     @FXML
     void backPressed(KeyEvent event) throws Exception
     {
         ManagerController.handleAction(null, event, "ManagerMainMenu.fxml");
     }
 
+    /**
+     * event handler for logout button click
+     * @param event mouns event
+     * @throws Exception for load scene
+     */
     @FXML
     void logoutClicked(MouseEvent event) throws Exception
     {
         ManagerController.handleAction(event, null, "Login.fxml");
     }
 
+    /**
+     * event handler for logout button press
+     * @param event keyboard event
+     * @throws Exception for load scene
+     */
     @FXML
     void logoutPressed(KeyEvent event) throws Exception
     {
         ManagerController.handleAction(null, event, "Login.fxml");
     }
+  
 
-
-
-
+    /**
+     * Initilaizer to setup the scene 
+     */
     @FXML
     private void initialize()
     {
@@ -105,6 +174,11 @@ public class ManagerHireController {
     }
 
 
+    /**
+     * Handles after the employee add button is clicked. Controls the username, validates the inputs
+     * @param event mouse event
+     * @throws Exception for load scene
+     */
     @FXML
     void addClicked(MouseEvent event) throws Exception
     {
@@ -138,6 +212,11 @@ public class ManagerHireController {
         }
     }
 
+    /**
+     * Capitalizes the first letter of the name and surname
+     * @param input name or surname
+     * @return first letter capitalized string
+     */
     private String capitalizeFirstLetter(String input)
     {
         if (input == null || input.isEmpty())
@@ -146,6 +225,11 @@ public class ManagerHireController {
         return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
     }
 
+    /**
+     * Control cases for passwords
+     * @param password password to be controlled
+     * @return true if the conditions satisfied, else false
+     */
     private boolean checkPassword(String password)
     {
         if (password == null || password.trim().isEmpty())
@@ -176,6 +260,11 @@ public class ManagerHireController {
         return true;
     }
 
+    /**
+     * Control cases for username
+     * @param username username to be controlled
+     * @return true if the conditions satisfied, else false
+     */
     private boolean checkUsername(String username)
     {
         if (username == null || username.trim().isEmpty())
@@ -200,6 +289,11 @@ public class ManagerHireController {
         return true;
     }
 
+    /**
+     * Control cases for surname
+     * @param surname surname to be controlled
+     * @return true if the conditions satisfied, else false
+     */
     private boolean checkSurname(String surname)
     {
         if (surname == null || surname.trim().isEmpty())
@@ -218,6 +312,11 @@ public class ManagerHireController {
         return true;
     }
 
+    /**
+     * Control cases for name
+     * @param name name to be controlled
+     * @return true if the conditions satisfied, else false
+     */
     private boolean checkName(String name)
     {
         if (name == null || name.trim().isEmpty())
