7053708e1644819cffdaa48b78824442d0eb44d0
MuratAlkaptan <131607763+MuratAlkaptan@users.noreply.github.com>
2025-01-13 08:26:21 +0300

    Update Sessions.java
    Sessions Javadoc added

--- src/Sessions.java
+++ src/Sessions.java
@@ -8,12 +8,16 @@ public class Sessions {
     private Date date;
     private Time time;
 
-    public Sessions() {
-        // Default constructor
-    }
-
-    public Sessions(int id, int hallId, int movieId, Date date, Time time)
-    {
+    
+    /**
+     * Full constructor for Sessions
+     * @param id
+     * @param hallId
+     * @param movieId
+     * @param date
+     * @param time
+     */
+    public Sessions(int id, int hallId, int movieId, Date date, Time time) {
         this.id = id;
         this.hallId = hallId;
         this.movieId = movieId;
@@ -21,69 +25,114 @@ public class Sessions {
         this.time = time;
     }
 
-    // overloaded constructor
-    public Sessions(int hallId, int movieId, Date date, Time time)
-    {
+    /**
+     * Constructor for Sessions without id for flexibility
+     * @param hallId
+     * @param movieId
+     * @param date
+     * @param time
+     */
+    public Sessions(int hallId, int movieId, Date date, Time time) {
         this.hallId = hallId;
         this.movieId = movieId;
         this.date = date;
         this.time = time;
     }
 
-    // Copy Constructor
-    public Sessions(Sessions original)
-    {
-        this.id = original.id;
-        this.hallId = original.hallId;
-        this.movieId = original.movieId;
-        this.date = original.date;
-        this.time = original.time;
+    /**
+     * Default constructor for Sessions
+     * @param hallId
+     * @param movieId
+     * @param time
+     */
+    public Sessions() {
+        
     }
 
-    // Getters
+    /**
+     * Getter for ID
+     * @return
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Getter for Hall ID
+     * @return
+     */
     public int getHallId() {
         return hallId;
     }
 
+    /**
+     * Getter for Movie ID
+     * @return
+     */
     public int getMovieId() {
         return movieId;
     }
 
+    /**
+     * Getter for Date
+     * @return
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * Getter for Time
+     * @return
+     */
     public Time getTime() {
         return time;
     }
 
-
-    // Setters
+    /**
+     * Setter for ID
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Setter for Hall ID
+     * @param hallId
+     */
     public void setHallId(int hallId) {
         this.hallId = hallId;
     }
 
+    /**
+     * Setter for Movie ID
+     * @param movieId
+     */
     public void setMovieId(int movieId) {
         this.movieId = movieId;
     }
 
+    /**
+     * Setter for Date
+     * @param date
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * Setter for Time
+     * @param time
+     */
     public void setTime(Time time) {
         this.time = time;
     }
 
-
+    /**
+     * toString method for Sessions
+     * @return Sessions as a string
+     */
     @Override
     public String toString() {
         return "Sessions{" +
